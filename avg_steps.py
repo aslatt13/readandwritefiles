61691387c9165fff0456447802f1e03593e2ad9d
aslatt13 <aslatt13@gmail.com>
2022-09-08 22:04:05 -0500

    added steps.py

--- avg_steps.py
+++ avg_steps.py
@@ -12,19 +12,30 @@ def main():
 
     next(csvfile)
 
+    steps = [0]
+    days = [0]
+    month = 1
+
+    for row in csvfile:
+        if int(row[0]) != month:
+            month += 1
+            steps.append(0)
+            days.append(0)
+        steps[month-1] += int(row[1])
+        days[month-1] += 1
+
+    infile.close()
+        
+
     outfile.write('Month, Avg Steps\n')
 
+    month_list = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
-
-    for record in csvfile:
-        month = record[0]
-        
-        avg_steps = 0
-
-        outfile.write(month + ',' + avg_steps + '\n')
+    for month in range(len(days)):
+        avg_steps = steps[month] / days[month]
+        outfile.write(month_list[month] + ',' + '{:0.2f}'.format(avg_steps) + '\n')
 
     outfile.close()
-
 
 
 
